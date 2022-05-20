5b89a657daa8a6fd7df5aebcfe73b98d1d9b3493
Lucas Aboud <lucas_aboud@hotmail.com>
2022-05-20 10:49:18 -0300

    pequenas correções

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
+const routes = require('./src/routes');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src', 'views'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
