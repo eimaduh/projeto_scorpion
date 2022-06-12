e2a5de4248081bd94f371ccc55619e4615a7f55c
Lucas Aboud <lucas_aboud@hotmail.com>
2022-06-12 14:05:06 -0300

    att pra dar merge

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./src/routes');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('views', path.join(__dirname, 'src', 'views',));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')))
