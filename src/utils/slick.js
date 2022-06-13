c40f20aeaa2cf3a96da4489fca778dbff928afe8
Lucas Aboud <lucas_aboud@hotmail.com>
2022-06-13 13:40:48 -0300

    subi algo

---
+++ src/utils/slick.js
@@ -0,0 +1,11 @@
+$('.carrossel').slick({
+    dots: yes,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $("bx bx-chevron-right"),
+    prevArrow: $("bx bx-chevron-left"),
+  });
+  
