fba592c63e1d24e641c59d9094ecb4434fd8c81e
eimaduh <maduhsilva402@gmail.com>
2022-07-20 11:20:31 -0300

    separando login e register, configurando carrionho

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,12 @@
 const loginController = {
     getLoginPage: (req, res) => {
         res.render('login')
+    },
+
+    getRegistrationPage: (req, res) => {  
+        res.render('register')
     }
+
 }
 
 module.exports = loginController
--- src/routes/login.js
+++ src/routes/login.js
@@ -3,5 +3,6 @@ const loginRouter = express.Router()
 const loginController = require('../controllers/loginController')
 
 loginRouter.get('/login', loginController.getLoginPage)
+loginRouter.get('/login/register', loginController.getRegistrationPage)
 
 module.exports = loginRouter
