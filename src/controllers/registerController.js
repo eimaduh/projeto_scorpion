435fe4d2a92fb5b0fb02e52bfd3c01d69319c637
Lucas Aboud <lucas_aboud@hotmail.com>
2022-08-17 16:52:45 -0300

    crud usuários

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -4,29 +4,30 @@ const bcrypt = require("bcrypt");
 
 const registerController = {
     getRegistrationPage: (req, res) => {
-        database.Register.findAll()
         res.render('register')
     },
     create: (req, res) => {
         const {
-         name,
-         email,
-         password,
+            first_name,
+            last_name,
+            email,
+            password,
         } = req.body;
 
         const passwordHash = bcrypt.hashSync(password, 10);
-      
-        database.Register.create({
-         name,
-         email,
-         password : passwordHash,
+
+        database.Users.create({
+            first_name,
+            last_name,
+            email,
+            password: passwordHash,
         })
-         .then(() => {
-          return res.redirect('/login');
-         })
-         .catch(error => res.send(error))
-       }
- 
+            .then(() => {
+                return res.redirect('/login');
+            })
+            .catch(error => res.send(error))
+    }
+
 
 
 }
