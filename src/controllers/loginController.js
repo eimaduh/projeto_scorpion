e23bae184831c0d555d00f177a67988e4f538991
eimaduh <maduhsilva402@gmail.com>
2022-08-18 15:14:33 -0300

    error login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,27 +4,31 @@ const bcrypt = require("bcrypt");
 
 const loginController = {
   getLoginPage: (req, res) => {
-    res.render('login')
+    res.render('login',  { error: ''})
   },
+
   loginUser: (req, res) => {
     const { email, password } = req.body
     database.Users.findOne({
       where: { email },
     }).then(user => {
       if (!user) {
-        alert("Este Usuário não existe!")
-        return res.redirect("/login")
+        return res.render("login", {
+          error: 'Email ou senha incorreta'
+        })
       }
 
       if (bcrypt.compareSync(password, user.password)) {
         req.session.usuarioLogado = user.email, user.id, user.name
         res.cookie('user', JSON.stringify({ id: user.id, name: user.name, email: user.email, type: user.type }));
-        return res.redirect("/users")
+        return res.render("login", {
+          error: 'Email ou senha incorreta'
+        })
       }
 
 
-      return res.redirect('login')
-    }).catch(error => console.log("error"))
+      return res.redirect('/')
+    }).catch(error => res.send(error.error))
   }
 }
 
