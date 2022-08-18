f781e27b833f155d2aa9f69a61ab8cf9de04097d
Lucas Aboud <lucas_aboud@hotmail.com>
2022-08-18 16:06:43 -0300

    nada de mais mas tem que ser feito

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -30,34 +30,4 @@ const loginController = {
   }
 }
 
-// const loginController = {
-//     getLoginPage: (req, res) => {
-//       res.render('login');
-//     },
-//     loginUser: (req, res) => {
-//       const { email, password } = req.body;
-
-//       database.Users.findOne({
-//         where: {
-//           email,
-//         },
-//       }).then(user => {
-
-//         if (!user) {
-//           alert('User does not exist');
-//           res.redirect('/login');
-//         }
-
-//         if (bcrypt.compare(password, user.password)) {
-//           res.cookie('user', JSON.stringify({ id: user.id, name: user.name, type: user.type }));
-
-//           res.redirect('/users');
-//         }
-
-//         res.render('/login');
-//       }).catch(error => console.log(error));
-//     }
-//   }
-
-
 module.exports = loginController
